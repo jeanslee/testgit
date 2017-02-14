eff8f8b0da9383c295e619e16216ebe76c95656b
lifei <sbnui2004@126.com>
2017-02-14 15:48:52 +0800

    second commit---Openwrt SDK

--- sh.sh
+++ sh.sh
@@ -1,10 +1,10 @@
 #   …   or create a new repository on the command line
 
 
-#echo "# testgit" >> README.md
+echo "# second commit is Openwrt SDK" >> README.md
 git init
 git add .
-git commit -m "first commit"
+git commit -m "second commit---Openwrt SDK"
 #git remote add origin https://github.com/jeanslee/testgit.git
 #git push -u origin master
 git push
